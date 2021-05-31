a0f1b66f0e22d233847cb5321cf27665240a7f69
mercy021 <kinyanjuimercywangari@gmail.com>
2021-05-31 15:37:08 +0300

    first commit

---
+++ moji.js
@@ -0,0 +1,9 @@
+const closedFace=document.querySelector(".closed");
+const openFace=document.querySelector(".open");
+
+closedFace.addEventListener("click",()=>{
+    if(openFace.classList.contains("open")){
+       openFace.classList.add("active"); 
+       closedFace.classList.remove("active"); 
+    }
+});
